fb9382c5dc0e796a8ad3e804a50caa527a90b213
Krunal281 <prajapatikrunal8410@gmail.com>
2026-05-26 18:22:22 +0530

    add frontend cors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://aadhya-shakti.netlify.app",
+  credentials: true
+}));
 // Set high size limits to comfortably support base64 product image uploads
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
